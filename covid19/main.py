52a3aeb8d4c03b415950311718b6cd9acb6a6548
Paul Molodowitch <paul@molodowitch.com>
2020-07-09 08:16:51 -0700

    move kelly_colors into constants

--- covid19/main.py
+++ covid19/main.py
@@ -17,43 +17,12 @@ import bokeh.layouts as lyt
 import bokeh.models as mdl
 import bokeh.plotting
 
-from collections import OrderedDict, namedtuple
+from collections import namedtuple
 
+from .constants import KELLY_COLORS
 from .entities import Country, County, Entity, State
 from .retrievers import DataCacheKey, EntityDataType
 
-
-################################################################################
-# Colors
-
-# Thanks to Kenneth Kelly + Ohad Schneider:
-# https://stackoverflow.com/a/13781114/920545
-kelly_colors_dict = OrderedDict(
-    black=(0,0,0),
-    vivid_yellow=(255, 179, 0),
-    strong_purple=(128, 62, 117),
-    vivid_orange=(255, 104, 0),
-    very_light_blue=(166, 189, 215),
-    vivid_red=(193, 0, 32),
-    grayish_yellow=(206, 162, 98),
-    medium_gray=(129, 112, 102),
-
-    # these aren't good for people with defective color vision:
-    vivid_green=(0, 125, 52),
-    strong_purplish_pink=(246, 118, 142),
-    strong_blue=(0, 83, 138),
-    strong_yellowish_pink=(255, 122, 92),
-    strong_violet=(83, 55, 122),
-    vivid_orange_yellow=(255, 142, 0),
-    strong_purplish_red=(179, 40, 81),
-    vivid_greenish_yellow=(244, 200, 0),
-    strong_reddish_brown=(127, 24, 13),
-    vivid_yellowish_green=(147, 170, 0),
-    deep_yellowish_brown=(89, 51, 21),
-    vivid_reddish_orange=(241, 58, 19),
-    dark_olive_green=(35, 44, 22),
-)
-kelly_colors = list(kelly_colors_dict.values())
 
 ################################################################################
 # Utilities
@@ -535,7 +504,7 @@ class View(object):
 
     def color(self, entity):
         i = self.model.entities.index(entity)
-        return kelly_colors[i % len(kelly_colors)]
+        return KELLY_COLORS[i % len(KELLY_COLORS)]
 
     def set_controller(self, controller):
         self.controller = controller
